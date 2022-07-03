adfa520b28d2c0fade199617f13512a146ef5296
Aleksey Vladimirov <alekseyvlad1miroff@yandex.ru>
2022-07-03 12:35:26 +0700

    WIP

--- main.py
+++ main.py
@@ -2,11 +2,37 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+goods = [{
+    "Name":"Snus-200mg",
+    "Desc":"Good-Snus",
+    "Price":"100$",
+},{
+    "Name":"Snus-100mg",
+    "Desc":"Bad-Snus",
+    "Price":"50$",
+   }
+    ,{
+    "Name":"Snus-20mg",
+    "Desc":"School-Snus",
+    "Price":"10$",}
+
+]
+
 @app.route('/')
-def index():
-    return render_template('index.html')
+def display_main_page():
+    return render_template('index.html', title = 'Главная')
+
+@app.route('/product')
+def display_product_page():
+    return render_template('index.html', title = 'О товаре')
+
+@app.route('/cart')
+def display_cart():
+    return render_template('index.html', title = 'Корзина')
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
 
 
